e323985ced970b143b10afccf2e3af71fc1cc279
Amandeep * <amandeep.1@globallogic.com>
2024-06-04 10:47:51 +0530

    first commit

---
+++ src/main/java/tripgo/glo/tripgo/TripgoApplication.java
@@ -0,0 +1,13 @@
+package tripgo.glo.tripgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tripgo/glo/tripgo/TripgoApplicationTests.java
@@ -0,0 +1,13 @@
+package tripgo.glo.tripgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
